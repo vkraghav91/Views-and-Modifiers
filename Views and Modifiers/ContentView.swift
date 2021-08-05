03f796dc4099a54aa3d3c953a9b1258511e9f88b
Varun Kumar Raghav <vrnrghv@gmail.com>
2021-08-05 10:40:09 +0530

    Initial project files are added

---
+++ Views and Modifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Views and Modifiers
+//
+//  Created by Varun Kumar Raghav on 05/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Views and Modifiers/Views_and_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Views_and_ModifiersApp.swift
+//  Views and Modifiers
+//
+//  Created by Varun Kumar Raghav on 05/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Views_and_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
